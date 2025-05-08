e38a36dbe0a0cba91faf73710bda5c621772dcdd
Falk3579 <202940408+Falk3579@users.noreply.github.com>
2025-05-08 13:30:22 +0200

    Update test_ptwDIAMENTOR.py

--- tests/instruments/ptw/test_ptwDIAMENTOR.py
+++ tests/instruments/ptw/test_ptwDIAMENTOR.py
@@ -31,7 +31,7 @@ def test_baudrate():
     """Verify the communication of the baudrate getter/setter."""
     with expected_protocol(
         ptwDIAMENTOR,
-        [("BR4", None),
+        [("BR4", "BR4"),
          ("BR", "BR0")],
     ) as inst:
         inst.baudrate = 115200
@@ -85,59 +85,92 @@ def test_is_eeprom_ok():
         assert inst.is_eeprom_ok is False
 
 
+@pytest.mark.parametrize("pressure", [500, 1500])
+def test_pressure(pressure):
+    """Verify the communication of the pressure getter/setter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [(f"PRE{pressure:04d}", f"PRE{pressure:04d}"),
+         ("PRE", f"PRE{pressure:04d}")]
+    ) as inst:
+        inst.pressure = pressure
+        assert inst.pressure == pressure
 
 
+def test_id():
+    """Verify the communication of the ID getter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [('PTW', 'CRS 1.2.4')],
+    ) as inst:
+        assert inst.id == 'CRS 1.2.4'
 
 
-
-# def test_id():
-    # """Verify the communication of the ID getter."""
-    # with expected_protocol(
-        # ptwDIAMENTOR,
-        # [('PTW', 'CRS 1.2.4')],
-    # ) as inst:
-        # assert inst.id == 'CRS 1.2.4'
-
-
-# @pytest.mark.parametrize("temperature", [0, 1, 30])
-# def test_temperature(temperature):
-    # """Verify the communication of the temperature getter/setter."""
-    # with expected_protocol(
-        # ptwDIAMENTOR,
-        # [(f"TMPA{temperature:02d}", f"TMPA{temperature:02d}"),
-         # ('TMPA', f"TMPA{temperature:02d}")]
-    # ) as inst:
-        # inst.temperature = temperature
-        # assert inst.temperature == temperature
+def test_measurement():
+    """Verify the communication of the measurement getter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [("M", "M9999.99,8888.88,222,33,eeee.ee,ffff.ff,mmm,ss,34567")],
+    ) as inst:
+        assert inst.measurement == {"dap": 9999.99,
+                                    "dap_rate": 8888.88,
+                                    "time": 13353,
+                                    "crc": 34567
+                                    }
 
 
-# @pytest.mark.parametrize("pressure", [500, 1500])
-# def test_pressure(pressure):
-    # """Verify the communication of the pressure getter/setter."""
-    # with expected_protocol(
-        # ptwDIAMENTOR,
-        # [(f"PRE{pressure:04d}", f"PRE{pressure:04d}"),
-         # ('PRE', f"PRE{pressure:04d}")]
-    # ) as inst:
-        # inst.pressure = pressure
-        # assert inst.pressure == pressure
+def test_serial_number():
+    """Verify the communication of the serial number getter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [("SER", "SER345678")]
+    ) as inst:
+        assert inst.serial_number == 345678
 
 
-# def test_serial_number():
-    # """Verify the communication of the serial number getter."""
-    # with expected_protocol(
-        # ptwDIAMENTOR,
-        # [("SER", "SER345678")]
-    # ) as inst:
-        # assert inst.serial_number == 345678
+@pytest.mark.parametrize("temperature", [0, 1, 30])
+def test_temperature(temperature):
+    """Verify the communication of the temperature getter/setter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [(f"TMPA{temperature:02d}", f"TMPA{temperature:02d}"),
+         ('TMPA', f"TMPA{temperature:02d}")]
+    ) as inst:
+        inst.temperature = temperature
+        assert inst.temperature == temperature
 
 
-# @pytest.mark.parametrize("dap_unit", [1, 2, 4])
-# def test_dap_unit(dap_unit):
-    # """Verify the communication of the dap_unit getter/setter."""
-    # with expected_protocol(
-        # ptwDIAMENTOR,
-        # [("U1", "U1")]
-    # ) as inst:
-        # inst.dap_unit = dap_unit
-        # assert inst.dap_unit == 345678
+def test_dap_unit():
+    """Verify the communication of the dap_unit getter/setter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [("U", "U1"),
+         ("U3", "U3")
+         ]
+    ) as inst:
+        assert inst.dap_unit == "cGycm2"
+        inst.dap_unit = "uGym2"
+
+
+def test_calibration_factor():
+    """Verify the communication of the calibration_factor getter/setter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [("KA4.3000E09", "KA4.3000E09"),
+         ("KA", "KA1.3356E11")
+         ]
+    ) as inst:
+        inst.calibration_factor = 4.3E9
+        assert inst.calibration_factor == 1.3356E11
+
+
+def test_correctrion_factor():
+    """Verify the communication of the correctrion_factor getter/setter."""
+    with expected_protocol(
+        ptwDIAMENTOR,
+        [("KFA", "KFA1.010"),
+         ("KFA3.870", "KFA3.870")
+         ]
+    ) as inst:
+        assert inst.correctrion_factor == 1.01
+        inst.correctrion_factor = 3.87
