2d4603e74359d75d28a13e357e264095e4a73926
Falk3579 <202940408+Falk3579@users.noreply.github.com>
2025-07-04 08:46:58 +0200

    Update spellmanXRV.py

--- pymeasure/instruments/spellmanhv/spellmanXRV.py
+++ pymeasure/instruments/spellmanhv/spellmanXRV.py
@@ -254,9 +254,7 @@ class SpellmanXRV(Instrument):
         if interface_type is InterfaceType.tcpip:
             self.checksum_enabled = False
 
-        # don't execute during tests
-        if self.adapter.connection is not MagicMock():
-            self.set_scaling()
+        self.set_scaling()
 
     def checksum(self, string_to_check):
         """Calculate the checksum.
